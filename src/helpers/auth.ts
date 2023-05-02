9c6bdd15e0789ac245464b771111c7034e41b6a1
Osmar Leandro <osmarleandrods@gmail.com>
2023-05-01 21:50:09 -0300

    ref: add auth helper

---
+++ src/helpers/auth.ts
@@ -0,0 +1,39 @@
+import { NextFunction, Request, Response } from 'express';
+import jwt from 'jsonwebtoken';
+
+import { readStringFromFile } from './read-file';
+
+export function getToken(req: Request): string | undefined {
+  const authHeader = req.headers["authorization"];
+
+  const token = authHeader?.split(" ")[1];
+
+  return token;
+}
+
+export async function verifyToken(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const secret = await readStringFromFile("secret.file");
+
+  if (!req.headers.authorization) {
+    return res.status(401);
+  }
+
+  const token = getToken(req);
+
+  if (!token) {
+    return res.status(401);
+  }
+
+  try {
+    const verified = jwt.verify(token, secret);
+    req.user = verified;
+    next();
+  } catch (err) {
+    console.log(err);
+    res.status(400).json({ message: "Invalid Token!" });
+  }
+}
--- src/helpers/get-token.ts
+++
@@ -1,9 +0,0 @@
-import { Request } from "express";
-
-export const getToken = (req: Request) => {
-  const authHeader = req.headers["authorization"];
-
-  const token = authHeader?.split(" ")[1];
-
-  return token;
-};
