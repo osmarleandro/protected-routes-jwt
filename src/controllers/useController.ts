7a08297c0ef5ab520a69894e3d5746873dc17e35
Osmar Leandro <osmarleandrods@gmail.com>
2023-05-01 21:32:36 -0300

    feat: create user then token

--- src/controllers/useController.ts
+++ src/controllers/useController.ts
@@ -1,6 +1,7 @@
 import { User } from "../entities/user";
 import { Request, Response } from "express";
 import bcrypt from "bcrypt";
+import { createUserToken } from "../helpers/create-user-token";
 
 export const createUser = async (req: Request, res: Response) => {
   const { name, login, password, confirmPass } = req.body;
@@ -14,16 +15,14 @@ export const createUser = async (req: Request, res: Response) => {
   if (password !== confirmPass)
     return res.status(422).json({ message: "The passwords are not equals!" });
 
-  const userExists = await User.findOne({ login: login });
+  const userExists = await User.findOne({ login: login }).exec();
 
   if (userExists)
     return res.status(422).json({ message: "User already exists!" });
 
-  // Create password
   const salt = await bcrypt.genSalt(12);
   const passwordHash = await bcrypt.hash(password, salt);
 
-  // Create user
   const user = new User({
     login,
     name,
@@ -31,8 +30,8 @@ export const createUser = async (req: Request, res: Response) => {
   });
 
   try {
-    await user.save();
-    res.status(201).json({ message: "User created with success!" });
+    const newUser = await user.save();
+    await createUserToken(newUser, req, res);
   } catch (err) {
     console.log(err);
     res.status(500).json({ message: "Something was wrong, try again!" });
