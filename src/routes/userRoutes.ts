87b5e6fbc206c7ebf189eda7841976643381b44a
Osmar Leandro <osmarleandrods@gmail.com>
2023-05-01 21:11:14 -0300

    feat: add new user route

--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,7 +1,11 @@
 import { Router } from "express";
+import { createUser } from "../controllers/useController";
 
 export const userRouter = Router();
 
 userRouter.get("/", (req, res) => {
   res.status(200).json({ message: "User Route!" });
 });
+
+
+userRouter.post("/", createUser);
